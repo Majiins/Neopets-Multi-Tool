93e165a3b5b907afcee9bda5f6ae062e80024087
Majiins <59684456+Majiins@users.noreply.github.com>
2020-01-28 12:39:24 +1100

    Add files via upload

--- classes/trudy.py
+++ classes/trudy.py
@@ -6,18 +6,19 @@ class Trudy:
         self.neo = neo
 
     def doTrudy(self, username):
+        ''' This will play Trudy's Surprise every 24 hours '''
         run = self.neo.setData(username, 'trudy')
         if time.time() - float(run) >= 86400:
             resp = self.neo.get('trudys_surprise.phtml?delevent=yes')
             if resp.find('&slt=1') > 1:
-                result = self.neo.getBetween(resp, '/trudydaily/slotgame.phtml?id=', '" name="')
+                ''' if slt=1 is found, we can play the game
+                    otherwise do nothing and update the data values '''
+                result = self.neo.trudyParse(resp, '/trudydaily/slotgame.phtml?id=', '" name="')
                 resp = self.neo.get('trudydaily/slotgame.phtml?id=%s' % result, 'http://www.neopets.com/trudys_surprise.phtml?delevent=yes')
-                results = self.neo.getBetween(resp, '\'key\': \'', '\'};')
+                results = self.neo.trudyParse(resp, '\'key\': \'', '\'};')
                 self.neo.post('trudydaily/ajax/claimprize.php', {'action': 'getslotstate', 'key': results}, 'http://www.neopets.com/trudydaily/slotgame.phtml?id=%s' % result)
                 resp = self.neo.post('trudydaily/ajax/claimprize.php', {'action': 'beginroll'}, 'http://www.neopets.com/trudydaily/ajax/claimprize.php')
                 for x in json.loads(resp)['prizes']:
                     self.neo.log('Trudy\'s Surprise: Won %sNP!' % x['value'])
                 self.neo.post('trudydaily/ajax/claimprize.php', {'action': 'prizeclaimed'}, 'http://www.neopets.com/trudydaily/ajax/claimprize.php')
-                self.neo.updateData(username, 'trudy', '%s:%s' % ('trudy', time.time()))
-        else:
-            self.neo.log('Already Done Trudy\'s Surprise Today')
+            self.neo.updateData(username, 'trudy', '%s:%s' % ('trudy', time.time()))
